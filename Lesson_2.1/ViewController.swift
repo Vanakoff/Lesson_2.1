ad864d454228fbe9877fc76d55abc3b6a1050abe
Vanakoff <andrey.vanakoff@gmail.com>
2026-01-08 02:09:46 +0100

    Initial Commit

---
+++ Lesson_2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2.1
+//
+//  Created by Andrey Vanakoff on 08/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
